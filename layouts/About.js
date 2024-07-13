30ef61cdf4c3bede805174bab3a8214aa9fee84c
Dimas <prawira.dimas.yudha@gmail.com>
2024-07-13 14:36:39 +0700

    hiding experiences and education in about page.

--- layouts/About.js
+++ layouts/About.js
@@ -29,38 +29,38 @@ const About = ({ data }) => {
           <MDXRemote {...mdxContent} components={shortcodes} />
         </div>
 
-        <div className="row mt-24 text-left lg:flex-nowrap">
-          <div className="lg:col-6 ">
-            <div className="rounded border border-border p-6 dark:border-darkmode-border ">
-              {markdownify(education.title, "h2", "section-title mb-12")}
-              <div className="row">
-                {education.degrees.map((degree, index) => (
-                  <div className="mb-7 md:col-6" key={"degree-" + index}>
-                    <h4 className="text-base lg:text-[25px]">
-                      {degree.university}
-                    </h4>
-                    <p className="mt-2">{degree.content}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="experience mt-10 lg:mt-0 lg:col-6">
-            <div className="rounded border border-border p-6 dark:border-darkmode-border ">
-              {markdownify(experience.title, "h2", "section-title mb-12")}
-              <ul className="row">
-                {experience?.list?.map((item, index) => (
-                  <li
-                    className="mb-5 text-lg font-bold text-dark dark:text-darkmode-light lg:col-6"
-                    key={"experience-" + index}
-                  >
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
+        {/*<div className="row mt-24 text-left lg:flex-nowrap">*/}
+        {/*  <div className="lg:col-6 ">*/}
+        {/*    <div className="rounded border border-border p-6 dark:border-darkmode-border ">*/}
+        {/*      {markdownify(education.title, "h2", "section-title mb-12")}*/}
+        {/*      <div className="row">*/}
+        {/*        {education.degrees.map((degree, index) => (*/}
+        {/*          <div className="mb-7 md:col-6" key={"degree-" + index}>*/}
+        {/*            <h4 className="text-base lg:text-[25px]">*/}
+        {/*              {degree.university}*/}
+        {/*            </h4>*/}
+        {/*            <p className="mt-2">{degree.content}</p>*/}
+        {/*          </div>*/}
+        {/*        ))}*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*  <div className="experience mt-10 lg:mt-0 lg:col-6">*/}
+        {/*    <div className="rounded border border-border p-6 dark:border-darkmode-border ">*/}
+        {/*      {markdownify(experience.title, "h2", "section-title mb-12")}*/}
+        {/*      <ul className="row">*/}
+        {/*        {experience?.list?.map((item, index) => (*/}
+        {/*          <li*/}
+        {/*            className="mb-5 text-lg font-bold text-dark dark:text-darkmode-light lg:col-6"*/}
+        {/*            key={"experience-" + index}*/}
+        {/*          >*/}
+        {/*            {item}*/}
+        {/*          </li>*/}
+        {/*        ))}*/}
+        {/*      </ul>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </section>
   );
